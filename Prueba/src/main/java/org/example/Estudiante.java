9b76417d431313d779cc7965e989a04fae764443
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-09-14 16:12:07 -0600

    add models

---
+++ Prueba/src/main/java/org/example/Estudiante.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Estudiante extends Persona{
+
+    private String estado;
+
+    public Estudiante(int id, String nombre, String apellido, String fechaDeNacimiento, String estado) {
+        super(id, nombre, apellido, fechaDeNacimiento);
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
